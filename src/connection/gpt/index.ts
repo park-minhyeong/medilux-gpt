0307db6ec27f730d07d55d93f8e8522e23bb6799
minhyeong <minhyeong.dev@gmail.com>
2024-10-23 01:25:51 +0900

    [Feat] connect to gptApi

---
+++ src/connection/gpt/index.ts
@@ -0,0 +1,31 @@
+import httpRequest from "../axios";
+import { CreateGptRequest } from "@/interface/Gpt";
+
+const api = httpRequest.gpt("v1");
+
+async function postChat(message: string, apiKey: string) {
+  return await api.post<CreateGptRequest>(
+    "/chat/completions",
+    {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: message,
+        },
+      ],
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+    },
+  );
+}
+
+const gptApi = {
+  post: postChat,
+};
+
+export default gptApi;
