1b8703df9d3b9c89219d85628f9c70674cd6f54f
minhyeong <minhyeong.dev@gmail.com>
2024-10-23 13:14:01 +0900

    [Refactor]

--- src/components/chat/Page.tsx
+++ src/components/chat/Page.tsx
@@ -5,19 +5,22 @@ import { cn } from "fast-jsx/util";
 import useChat from "@/components/chat/useChat";
 import Action from "fast-jsx/layout/template/Action";
 import { useActionStore } from "fast-jsx/store";
+import MessageMolecule from "./molecule/Message.molecule";
 
 export default function Chat() {
-  const [message, setMessage] = useState<string>();
+  const [text, setText] = useState<string>();
   const { setModal } = useActionStore();
-  const { messages, postChat } = useChat();
+  const { messages, setMessage, postChat } = useChat();
   const container = {
     positions: "relative",
     sizes: "w-full min-h-screen",
     pressures: "px-1.5",
   };
   const body = {
+    displays: "flex flex-col gap-y-3.5",
     sizes: "w-full h-92 xs:h-100 sm:h-120 md:h-200",
     styles: "overflow-y-scroll",
+    boundaries: "border-2 rounded-md p-3.5 border-green-dark",
   };
   return (
     <Action.Show
@@ -48,23 +51,31 @@ export default function Chat() {
         />
         <div className={cn(body)}>
           {messages.map((message) => (
-            <div key={message} className="text-4xl">
-              {message}
-            </div>
+            <MessageMolecule
+              key={message.text}
+              message={message.text}
+              option={{
+                isMine: message.user === "user",
+              }}
+            />
           ))}
         </div>
         <div className="fixed left-0 bottom-0 w-full px-1.5 pb-3.5">
           <Input
-            state={[message, setMessage]}
+            state={[text, setText]}
             option={{
               height: "h-20",
               font: "text-2xl",
             }}
             onKeyDown={(e) => {
-              if (!message) return;
+              if (!text) return;
               if (e.key === "Enter") {
-                postChat(message);
-                return setMessage("");
+                setMessage({
+                  user: "user",
+                  text,
+                });
+                postChat(text);
+                return setText("");
               }
             }}
           />
