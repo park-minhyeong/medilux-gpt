46cce44ae86d9c3dd1f513908f60472d2335f511
minhyeong <minhyeong.dev@gmail.com>
2024-10-23 17:04:31 +0900

    [Feat] create ```isLoading```

--- src/hook/useSIgn.ts
+++ src/hook/useSIgn.ts
@@ -1,13 +1,68 @@
-import { useState } from "react";
+import { signApi } from "@/connection";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface SignStoreProps {
+  isSignIn: boolean;
+  signIn: () => void;
+  isLoading: boolean;
+  setIsLoading: (isLoading: boolean) => void;
+}
+const useSignStore = create(
+  persist<SignStoreProps>(
+    (set) => ({
+      isSignIn: false,
+      signIn: () => set({ isSignIn: true }),
+      isLoading: true,
+      setIsLoading: (isLoading) => set({ isLoading }),
+    }),
+    {
+      name: "signStorage",
+      onRehydrateStorage: () => (state) => {
+        state?.setIsLoading(true);
+      },
+    }
+  )
+);
 
 export default function useSign() {
-  const [username, setUsername] = useState<string>();
-  const [password, setPassword] = useState<string>();
-
+  const router = useRouter();
+  const {
+    isSignIn,
+    isLoading,
+    setIsLoading,
+    signIn: signInPersist,
+  } = useSignStore();
+  const { mutate: signIn } = useMutation({
+    mutationKey: ["signIn"],
+    mutationFn: ({
+      username,
+      password,
+    }: {
+      username: string;
+      password: string;
+    }) =>
+      signApi.signIn({
+        username,
+        password,
+      }),
+    onError: () => {
+      alert("회원 정보가 일치하지 않습니다.");
+    },
+    onSuccess: () => {
+      signInPersist();
+      router.push("/chat");
+    },
+  });
+  useEffect(() => {
+    setIsLoading(false);
+  }, [isSignIn]);
   return {
-    username,
-    setUsername,
-    password,
-    setPassword,
+    isSignIn,
+    isLoading,
+    signIn,
   };
 }
