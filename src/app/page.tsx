9055af8e40f349f844c6662f89e05d53b1f58e71
minhyeong <minhyeong.dev@gmail.com>
2024-10-23 17:03:43 +0900

    [Feat] create root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+"use client";
 import Home from "@/components/home/Page";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 export default function Page() {
-  return <Home />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+  );
 }
