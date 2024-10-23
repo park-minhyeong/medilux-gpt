d99fe1b9575f99bd76e3c61ce03fdf519c113a70
minhyeong <minhyeong.dev@gmail.com>
2024-10-23 22:27:38 +0900

    [Refactor]

--- src/components/chat/useChat.ts
+++ src/components/chat/useChat.ts
@@ -2,19 +2,27 @@ import { useMutation } from "@tanstack/react-query";
 import { chatApi } from "@/connection";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-
-interface Message {
-  user: "user" | "bot";
-  text: string;
-}
+import { Message } from "@/interface/Gpt";
+import defaultText from "@/asset/sample";
 interface ChatStoreProps {
   messages: Message[];
+  clearMessages: () => void;
   setMessage: (message: Message) => void;
+  prompt: string;
+  setPrompt: (prompt: string) => void;
 }
 const useChatStore = create(
   persist<ChatStoreProps>(
     (set, get) => ({
       messages: [],
+      clearMessages: () => {
+        set((state) => {
+          return {
+            ...state,
+            messages: [],
+          };
+        });
+      },
       setMessage: (message: Message) => {
         set((state) => {
           return {
@@ -23,30 +31,44 @@ const useChatStore = create(
           };
         });
       },
+      prompt: defaultText,
+      setPrompt: (prompt: string) => set({ prompt }),
     }),
     { name: "chatStorage" },
   ),
 );
 
 export default function useChat() {
-  const { messages, setMessage } = useChatStore();
+  const { messages, setMessage, prompt, setPrompt, clearMessages } =
+    useChatStore();
   const { mutate: postChat } = useMutation({
     mutationKey: ["postChat"],
-    mutationFn: (text: string) => {
-      const temps = [...messages, { user: "user", text }];
+    mutationFn: (content: string) => {
+      const temps: Message[] = [
+        ...messages,
+        { role: "user", content },
+        { role: "system", content: prompt },
+      ];
       return chatApi.post({
         messages: temps.map((prop) => ({
-          role: prop.user === "user" ? "user" : "assistant",
-          content: prop.text,
+          role: prop.role,
+          content: prop.content,
         })),
       });
     },
     onSuccess: (res) => {
       setMessage({
-        user: "bot",
-        text: res.choices[0].message.content,
+        role: "assistant",
+        content: res.choices[0].message.content,
       });
     },
   });
-  return { messages, setMessage, postChat };
+  return {
+    prompt,
+    setPrompt,
+    messages,
+    setMessage,
+    postChat,
+    clearMessages,
+  };
 }
