2521f513aeeaffd5901cc5ca523446be030d95f9
minhyeong <minhyeong.dev@gmail.com>
2024-11-01 23:41:00 +0900

    [Chore] supported by

--- src/components/home/molecule/Header.molecule.tsx
+++ src/components/home/molecule/Header.molecule.tsx
@@ -1,16 +1,26 @@
 import { cn } from "fast-jsx/util";
+import Image from "next/image";
 
 export default function HeaderMolecule() {
   const container = {
     positions: "absolute top-0 left-0",
   };
   const body = {
-    boundaries: "p-5",
-    fonts: "text-white text-2xl",
+    boundaries: "pl-5",
+    displays: "flex items-center gap-x-3.5",
+    fonts: "text-white text-3xl",
   };
   return (
     <div className={cn(container)}>
-      <div className={cn(body)}>메디럭스 X 블루시그넘</div>
+      <div className={cn(body)}>
+        <div>Supported By</div>
+        <Image
+          src="/images/blue-signum.svg"
+          alt="blue-signum"
+          width={120}
+          height={120}
+        />
+      </div>
     </div>
   );
 }
